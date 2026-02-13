45f1c4b956f6b728039e47fa0b925c9fde50aab5
rushi-dev24 <rushi-dev24@outlook.com>
2026-02-13 21:57:43 +0530

    Implement method to find second largest element

---
+++ _01_array_traversal/SecondLarge.java
@@ -0,0 +1,18 @@
+package _01_array_traversal;
+
+public class SecondLarge {
+	public static void secondLargeElement(int[] array) {
+		if(array==null || array.length<2) return;
+		int firMax=Integer.MIN_VALUE,secMax=Integer.MIN_VALUE,len=array.length;
+		for(int i=1;i<len;i++) {
+			if(firMax<array[i]) {
+				secMax=firMax;
+				firMax=array[i];
+			}
+			else if(array[i]>secMax && array[i]!=firMax) {
+				secMax=array[i];
+			}
+		}
+		System.out.println("First Max "+firMax+"\nSecond max "+secMax);
+	}
+}
