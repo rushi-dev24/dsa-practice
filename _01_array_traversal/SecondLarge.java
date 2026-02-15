d8f30d1d3b7eea4eab9737fb8619f8084bfa32ff
rushi-dev24 <rushi-dev24@outlook.com>
2026-02-15 06:52:33 +0530

    Fix loop to start from index 0 for array traversal

--- _01_array_traversal/SecondLarge.java
+++ _01_array_traversal/SecondLarge.java
@@ -4,7 +4,7 @@ public class SecondLarge {
 	public static void secondLargeElement(int[] array) {
 		if(array==null || array.length<2) return;
 		int firMax=Integer.MIN_VALUE,secMax=Integer.MIN_VALUE,len=array.length;
-		for(int i=1;i<len;i++) {
+		for(int i=0;i<len;i++) {
 			if(firMax<array[i]) {
 				secMax=firMax;
 				firMax=array[i];
@@ -12,6 +12,9 @@ public class SecondLarge {
 			else if(array[i]>secMax && array[i]!=firMax) {
 				secMax=array[i];
 			}
+		}
+		if(secMax==Integer.MIN_VALUE){
+			secMax=-1;
 		}
 		System.out.println("First Max "+firMax+"\nSecond max "+secMax);
 	}
