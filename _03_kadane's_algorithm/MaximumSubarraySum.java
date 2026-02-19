833de942eafcd11c22a42064994d29f59b04ce06
rushi-dev24 <rushi-dev24@outlook.com>
2026-02-19 22:06:21 +0530

    MaximumSubarraySum to find the maximum sum of all possible subarrays

---
+++ _03_kadane's_algorithm/MaximumSubarraySum.java
@@ -0,0 +1,11 @@
+public class MaximumSubarraySum{
+    public int maximumSubarraySum(int[] nums){
+        if(nums == null || nums.length == 0) return 0;
+        int n=nums.length,globalSum=Integer.MIN_VALUE,currentSum=0;
+        for(int i=0;i<n;i++){
+            currentSum=Math.max(currentSum+nums[i],nums[i]);
+            globalSum=Math.max(globalSum,currentSum);
+        }
+        return globalSum;
+    }
+}
