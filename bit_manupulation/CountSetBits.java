9a2cd9e91106b43113e6606a56680c64d1db5203
rushi-dev24 <rushi-dev24@outlook.com>
2026-03-17 17:12:08 +0530

    Create CountSetBits.java

---
+++ bit_manupulation/CountSetBits.java
@@ -0,0 +1,12 @@
+package bit_manupulation;
+
+public class CountSetBits {
+	public int countSetBits(int n) {
+		int count=0;
+		while(n>0) {
+			if((n & 1) == 1)count++;
+			n>>=1;
+		}
+		return count;
+	}
+}
