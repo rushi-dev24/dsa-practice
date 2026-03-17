8f705f9c1492812c593943059910d241d94fd6ce
rushi-dev24 <rushi-dev24@outlook.com>
2026-03-17 17:24:48 +0530

    Update CountSetBits.java

--- bit_manupulation/CountSetBits.java
+++ bit_manupulation/CountSetBits.java
@@ -4,9 +4,17 @@ public class CountSetBits {
 	public int countSetBits(int n) {
 		int count=0;
 		while(n>0) {
-			if((n & 1) == 1)count++;
+			count+=n&1;
 			n>>=1;
 		}
 		return count;
+		//or Using turning off right most set bit
+		/*
+		while(n!=0){
+			n=n & (n-1);
+			count++;
+		}
+		return count;
+		*/
 	}
 }
